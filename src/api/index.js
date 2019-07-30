8cf6b1a9c3bb5b1a31d562eceea37a1f2fad3a57
Paul M Dorr <paul.mdorr@gmail.com>
2019-07-30 20:20:21 -0300

    Add support for ZEIT now deploy

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,12 @@
 const axios = require('axios')
+const { send } = require('micro')
 /* 
   Since there's not much to do to get the news, this API just exposes
   a single function to process the request. Otherwise I would use a
   different file to process the routes and one function for each endpoint here.
 */
-module.exports.processReq = async function(req, res) {
-  const { category } = req.params
+module.exports = async function(req, res) {
+  const { category } = req.query
   let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${
     process.env.NEWS_API_KEY
   }`
@@ -15,8 +16,7 @@ module.exports.processReq = async function(req, res) {
   }
 
   const response = await axios.get(url)
+  const articles = response ? response.data.articles : []
 
-  return res.json({
-    articles: response ? response.data.articles : [],
-  })
+  send(res, response.status, { articles })
 }
