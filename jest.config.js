233089e9aa41241cfe2a199286159348679851d1
Paul M Dorr <paul.mdorr@gmail.com>
2019-08-03 14:25:00 -0300

    Add coverage ignore for styled components

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,18 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transform: { '^.+\\.tsx?$': 'babel-jest' },
   snapshotSerializers: ['jest-emotion'],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+    './src/components/': {
+      branches: 100,
+      statements: 100,
+    },
+  },
+  coveragePathIgnorePatterns: ['/node_modules/', './src/components/.*?/styled'],
 }
